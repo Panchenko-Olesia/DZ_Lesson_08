20380e453277c7183a310d93e391172e71b269ec
Olesia Panchenko <olesya.aht@gmail.com>
2023-05-23 13:52:41 +0300

    Commit_03

--- mane.py
+++ mane.py
@@ -1,3 +1,5 @@
 def mane_function(national):
     if national == 'ukrainian':
-    print("Все буде Украіна!")
+        print("Все буде Украіна!")
+    elif national == 'russian':
+        print('Russian ship go ...')
